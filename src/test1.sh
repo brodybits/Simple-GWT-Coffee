0aa1ff72674f50913302ea3cf7637001ba767d23
Chris Brody <chris.brody@gmail.com>
2012-10-11 23:16:54 +0200

    Rename Simple-GWT src files

--- src/test1.sh
+++ src/test1.sh
@@ -1,4 +1,4 @@
-cat js.start base1.js dom1.js dom2.js ui1.js ui2.js ui3.js ui4.js ui5.js js.end dp1.coffee render1.coffee > test1.coffee
+cat js.start Base.js SGWT-DOM-1.js SGWT-DOM-2.js SGWT-UI-1.js SGWT-UI-2.js SGWT-UI-3.js SGWT-UI-4.js SGWT-UI-5.js js.end SGWT-UI-DockPanel.coffee render-test-1.coffee > test1.coffee
 coffee -c test1.coffee
 cat test1.head test1.js test1.tail > test1.html
 
