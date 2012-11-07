b61d1daf8c9e8dcf057a892025c9a291538bd0ed
Chris Brody <chris.brody@gmail.com>
2012-11-07 14:41:20 +0100

    Removing old AJS-Class.js from test rendering scripts

--- test/make-simpletest1.sh
+++ test/make-simpletest1.sh
@@ -1,4 +1,4 @@
-cat js.start ../src/AJS-Class.js ../src/SGWT-Base.js ../src/SGWT-DOM-*.js ../src/SGWT-UI-*.js js.end simpletest1.coffee > test1.coffee
+cat js.start ../src/SGWT-Base.js ../src/SGWT-DOM-*.js ../src/SGWT-UI-*.js js.end simpletest1.coffee > test1.coffee
 coffee -c test1.coffee
 cat render-test-1.head test1.js render-test-1.tail > test1.html
 
